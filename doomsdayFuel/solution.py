0c6ee4f273110e057f4364782e08556afa05c881
thirteenpylons <thirteenpylons@gmail.com>
2021-12-17 10:58:26 -0500

    doomsday

--- doomsdayFuel/solution.py
+++ doomsdayFuel/solution.py
@@ -38,10 +38,54 @@ So, putting that together, and making a common denominator, gives an answer in t
 Author: Christian M. Fulton
 Date: 10.Dec.2021
 """
+import numpy as np
+import fractions as f
+
+
+def standard_form(matrix):
+    rows, columns = matrix.shape
+    swaps = 0
+    to_swap = []
+
+    for i in range(rows):
+        if not np.any(matrix[i]):
+            matrix = np.row_stack((matrix[i], matrix))
+            matrix = np.delete(matrix, i+1, 0)
+            swaps += 1
+        else:
+            to_swap.append(i)
+            matrix[i] /= matrix[i].sum()
+
+    aux = matrix.copy()
+    for counter, i in enumerate(to_swap):
+        matrix = np.column_stack((matrix, aux[:, i]))
+        matrix = np.delete(matrix, i-counter, 1)
+    for i in range(swaps):
+        matrix[i, i] = 1
+    return matrix, swaps
+
 
 def solution(m):
-    """
-    Parameter m: The matrix
-    Precondition: a matrix no larger than 10 by 10
-    """
-    NotImplementedError
+    states, swaps = standard_form(np.matrix(m, dtype=float))
+    FR = states
+    if len(states) > 1:
+        Q = states[swaps:, swaps:]
+        I = np.diag(np.ones(len(Q)))
+        R = states[swaps:, :swaps]
+        F = (I-Q)**(-1)
+        FR = F*R
+
+    result = map(f.Fraction, FR[0].A1.tolist())
+    denominators = []
+    for i in range(len(result)):
+        result[i] = result[i].limit_denominator(100)
+        denominators.append(result[i].denominator)
+    lcm = np.lcm.reduce(denominators)
+    numerators = [
+        result[i].numerator * lcm / result[i].denominator
+        for i in range(len(result))
+    ]
+
+    numerators.append(lcm)
+
+    return numerators
